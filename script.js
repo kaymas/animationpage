911e7b0ba4d41ea52845646a5212b3d7d042782b
kaymas <samyakahuja24@hotmail.com>
2017-12-27 14:03:46 +0530

    added more slides

--- script.js
+++ script.js
@@ -8,4 +8,15 @@ $(window).on('load', function(){
     $(this).toggleClass('is-transitioned');
   });
 
+  $('.example-3 .btn').on('click',function(){
+    $(this).parents('.layout').find('.alert').toggleClass('is-open');
+  });
+
+  $('.example-5 .send').on('click',function(){
+    $(this).parents('.email').addClass('is-sent');
+    setTimeout(function(){
+      $('.email').removeClass('is-sent');
+    },2000);
+  });
+
 });
